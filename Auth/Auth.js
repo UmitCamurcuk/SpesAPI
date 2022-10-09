c8d0591d8411216fea2d22e9e36cafe69907b290
umitcamurcuk <umtcmrck@icloud.com>
2022-10-10 00:06:08 +0300

    ChangeAuthVaribales

--- Auth/Auth.js
+++ Auth/Auth.js
@@ -2,22 +2,28 @@ const router = require('express').Router();
 const userModel = require('../Models/UserModel')
 const jwt = require('jsonwebtoken');
 
-router.post('/login', async (req,res) => {
+router.post('/login', async (req, res) => {
     //Check if user Exist
     const user = req.body;
 
-    const userExist = await userModel.findOne({UserName : user.UserName})
-    if(!userExist) return res.status(400).send({Error : true , Code : 400 , Message : "Username is not exist"})
+    const userExist = await userModel.findOne({ UserName: user.username })
+    if (!userExist) return res.status(400).send({ Error: true, Code: 400, Message: "Username is not exist" })
 
-    if(userExist.Password != user.Password) return res.status(400).send('Password is not correct')    
+    if (userExist.Password != user.password) return res.status(400).send('Password is not correct')
     user.Role = userExist.Role;
     const accessToken = jwt.sign(
         user,
         process.env.ACCESS_TOKEN_SECRET,
-        { expiresIn :  process.env.TOKEN_EXPIRE_TIME }
-        )
-    res.status(200).send({Error: false , Code : 200 , Message: 'Login Success', AccessToken : accessToken})
-    
+        { expiresIn: process.env.TOKEN_EXPIRE_TIME }
+    )
+    res.status(200).send({
+        Error: false,
+        Code: 200,
+        Message: 'Login Success',
+        AccessToken: accessToken,
+        User : 'Umit Camurcuk'
+    })
+
 })
 
 
